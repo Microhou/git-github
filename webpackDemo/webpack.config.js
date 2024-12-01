06cd051da5c6e9b3e70b483eae68348bf97a96be
anying <372931530@qq.com>
2024-12-01 21:43:37 +0800

    webpack and vite

---
+++ webpackDemo/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  } 
+}
